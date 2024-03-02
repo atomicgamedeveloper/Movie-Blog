449dd89dc4cee1756eca84b810b1e356c4066674
atomicgamedev <enokmalik@gmail.com>
2024-03-02 21:21:31 +0100

    Add project files.

---
+++ MovieBloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieBloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<BlogPost> Blogs { get; set; }
+    }
+}
